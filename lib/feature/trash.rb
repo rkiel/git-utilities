822700b349da5ddec927680ea572ab2cd814cf3c
rkiel <cygnus@pobox.com>
2017-02-25 10:18:46 -0500

    trash: added safety feature trash

--- lib/feature/trash.rb
+++ lib/feature/trash.rb
@@ -4,19 +4,27 @@ module Feature
 
   class Trash < Feature::Base
     def valid?
-      argv.size == 1
+      argv.size == 2
     end
 
     def help
-      "feature trash"
+      "feature trash confirmation-branch"
     end
 
     def execute
       parts = parse_branch(current_branch)
 
+      if argv.size == 2
+        confirmation_branch = argv[1]
+      else
+        confirmation_branch = ''
+      end
+
       standard_branch = parts[:standard]
       feature_branch  = current_branch
 
+      error "missing confirmation of branch: #{feature_branch}" if confirmation_branch == ''
+      error "confirmation branch does not match current branch: #{confirmation_branch} vs #{feature_branch}" if feature_branch != confirmation_branch
       error "invalid feature branch: #{feature_branch}" if standard_branches.include? feature_branch
 
       git_checkout standard_branch
