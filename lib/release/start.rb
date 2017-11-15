d94948aae196ea987d9c7d600de1442dae47f70e
Robert Kiel <cygnus@pobox.com>
2017-11-15 14:15:53 -0500

    Update start.rb

--- lib/release/start.rb
+++ lib/release/start.rb
@@ -4,19 +4,21 @@ module Release
 
   class Start < Release::Base
     def valid?
-      argv.size == 3 or argv.size == 4
+      argv.size == 4 or argv.size == 6
     end
 
     def help
-      "release start (major|minor|patch) [from] version"
+      "release start (major|minor|patch) from version [using master]"
     end
 
     def execute
       case argv.size
-      when 3
-        subcommand, level, version = *argv
       when 4
         subcommand, level, verb, version = *argv
+        starting_branch = nil
+      when 6
+        subcommand, level, verb, version, verb2, starting_branch = *argv
+        validate_branch_is_master(starting_branch)
       end
 
       validate_version_format version
@@ -38,13 +40,16 @@ module Release
         error "unknow release level: #{level}"
       end
 
+      starting_branch = release_tag_from_version(version) unless starting_branch
+
       validate_version_does_not_exist new_version
       validate_release_branch_does_not_exist(release_branch_from_version(new_version))
 
-      git_local_branch_create release_branch_from_version(new_version), release_tag_from_version(version)
+      git_local_branch_create release_branch_from_version(new_version), starting_branch
       git_push_upstream(release_branch_from_version(new_version))
 
       update_package_json new_version, "#{new_version} started"
+      git_push(release_branch_from_version(new_version))
     end
 
     private
